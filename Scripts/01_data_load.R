271d40db152ff30fe9752770432643136fb68978
Anne Alarilla (tier0) <tier0\anne.alarilla@ec2amaz-yzaz13.tier0.prd.thf.aws>
2022-05-26 14:39:07 +0000

    initial commit of data download

--- Scripts/01_data_load.R
+++ Scripts/01_data_load.R
@@ -1,1 +1,25 @@
 ##Data download
+
+#Libraries 
+library(curl)
+library(tidyverse)
+library(data.table)
+library(here)
+library(aws.s3)
+library(janitor)
+
+#data download
+
+link <- 'https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/05/AmbSYS-for-2022-Apr.csv'
+
+destfile <- here::here('data', "ambsys.csv")
+curl_download(link, destfile = destfile)
+
+
+
+
+
+
+
+
+ 
