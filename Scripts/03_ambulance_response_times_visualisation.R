8001af78fb43d9618271f483678e4b3deda096d4
Anne Alarilla (tier0) <tier0\anne.alarilla@ec2amaz-yzaz13.tier0.prd.thf.aws>
2022-05-30 15:42:18 +0000

    initial time series graph

--- Scripts/03_ambulance_response_times_visualisation.R
+++ Scripts/03_ambulance_response_times_visualisation.R
@@ -5,6 +5,14 @@ library(tidyverse)
 library(rio)
 library(lubridate)
 library(ggplot2)
+library(tsibble)
+library(feasts)
+library(hms)
+library(tidyverse)
+library(ggtext)
+library(THFstyle)
+
+
 
 #Data load
 
@@ -15,45 +23,51 @@ amb_dta<-s3read_using(read.csv # Which function are we using to read
                    , object = 'amb_RT_clean.csv' # File to open
                    , bucket = buck) # Bucket name defined above
 
-#Convert vars into time variables
+#reformat for plots
 vars<-c(paste0("c1_",c("mean", "90thcent")),c(paste0("c1T_",c("mean", "90thcent"))),
 c(paste0("c2_",c("mean", "90thcent"))),c(paste0("c3_",c("mean", "90thcent"))),
 c(paste0("c4_",c("mean", "90thcent"))))
 
+
+
 amb_dta_plot<-amb_dta %>% 
-  mutate_at(vars,as.numeric)
+ pivot_longer(c(c1_mean:c4_90thcent), names_to = 'metric', values_to = 'resp_time')
+
+try<-amb_dta_plot %>% 
+  mutate(resp_time2=as.POSIXct(as.numeric(resp_time),origin = "1970-01-01", tz="GMT")) %>% 
+  mutate(resp_time2=format(resp_time2, format="%H:%M:%S")) %>% 
+  mutate(resp_time2=as_hms(resp_time2)) %>% 
+  mutate(date2=yearmonth(date))
+
+
+targ<-
+
+
+amb_ts_plot<-amb_dta_plot %>%
+  # filter(metric=="c1_mean") %>%
+  mutate(date=yearmonth(date))
+# 
+# amb_ts_plot= as_tsibble(amb_ts_plot,
+#                         index= date,
+#                         key= metric, 
+#                         validate=FALSE)
 
 
 
-
-try$c1_90thcent_lab <- as.POSIXct(as.numeric(amb_dta$c1_90thcent), origin = "1970-01-01", tz = "GMT") %>%
-  format("%H:%M:%S")
-try$c1_90thcent_lab2 <- as.duration(amb_dta_plot$c1_90thcent)
-
-
-try<-amb_dta[,c(1:8,17,18)]
-
-
-
-Y.minT <- as.POSIXct("06:00:00" , format = "%H:%M:%S")
-Y.maxT <- as.POSIXct("12:00:00" , format = "%H:%M:%S")
-Y.min <- as.numeric(Y.minT)
-Y.max <- as.numeric(Y.maxT)
-
-nums <- seq(from = Y.min, to = Y.max, length.out = 1+abs(2 * difftime(Y.maxT, Y.minT)[[1]]))
-
-#Create a vector of date labels
-# labels <- as.POSIXct(nums, origin = "1970-01-01", tz = "GMT") %>% format("%H:%M")
-
-
-try %>% 
-  ggplot(.,aes(x=date, y=as.numeric(c1_90thcent_lab), group=region, colour=region))+
-  geom_line()+
-  geom_point()+
-  
-  
-  
-  # scale_y_continuous(breaks = nums, labels = labels, limits = c(Y.min, Y.max))+ 
-  # scale_x_date(breaks = seq( min(try$date), max(try$date),by="8 weeks"),
-  #            date_labels = '%d %b %g')
+try %>%
+  filter(str_detect(metric, 'c1_')) %>%
+  ggplot(.,aes(x=date2, y=resp_time2, group=region, colour=region))+
+  geom_line(linetype='dashed')+
+  geom_point(size=1)+
+  annotate("segment", x = min(try$date2), xend = max(try$date2), 
+           y =00:07:00, yend = 00:07:00, alpha = .1,fill = "grey20")+
+  theme_THF()+
+  facet_grid(rows=vars(metric))+
+  theme(legend.text=element_text(size=11),
+        axis.text.x=element_text(size=14),
+        axis.text.y=element_text(size=11),
+        plot.caption = element_markdown(hjust=0, size=9),
+        plot.margin = unit(c(1,1.5,0.5,0.5), "cm"),
+        legend.margin=margin(0,0,0,0),
+        legend.box.margin=margin(-10,-10,-10,-10))
 
