d6c65b8b6ce76368b7ee33722cb0104984bc421f
Anne Alarilla (tier0) <tier0\anne.alarilla@ec2amaz-yzaz13.tier0.prd.thf.aws>
2022-05-31 12:21:08 +0000

    Splitting by trusts and regions

--- Scripts/02_ambulance_data_cleaning.R
+++ Scripts/02_ambulance_data_cleaning.R
@@ -23,11 +23,6 @@ list_org_codes_trust<-c("RX9", "RYC", "R1F", "RRU", "RX6", "RX7", "RYE", "RYD", 
 #by region
 list_org_codes_region<-c("Y63", "Y62","Y60", "Y61", "Y56", "Y59", "Y58")
 
-amb_dta_clean<-amb_dta_clean %>% 
-  filter(org_code %in% c(list_org_codes_region, "Eng")) %>% 
-  mutate(date=as.Date(paste0(year,"/",ifelse (month<10, paste0(0,month),month),"/",01))) 
-
-        
 names(amb_dta_clean)[6:7]<-c(paste0("c1_",c("mean", "90thcent")))
 names(amb_dta_clean)[8:9]<-c(paste0("c1T_",c("mean", "90thcent")))
 names(amb_dta_clean)[10:11]<-c(paste0("c2_",c("mean", "90thcent")))
@@ -35,13 +30,29 @@ names(amb_dta_clean)[12:13]<-c(paste0("c3_",c("mean", "90thcent")))
 names(amb_dta_clean)[14:15]<-c(paste0("c4_",c("mean", "90thcent")))
 
 
+amb_dta_regions<-amb_dta_clean %>% 
+  filter(org_code %in% c(list_org_codes_region, "Eng")) %>% 
+  mutate(date=as.Date(paste0(year,"/",ifelse (month<10, paste0(0,month),month),"/",01))) 
 
-#save data
+      
+#save regions data 
 #### save R objects from the environment directly to your s3 bucket
 buck <- 'thf-dap-tier0-projects-iht-067208b7-projectbucket-1mrmynh0q7ljp/ambulance/clean' ## my bucket name
 
-s3write_using(amb_dta_clean # What R object we are saving
+s3write_using(amb_dta_regions # What R object we are saving
               , FUN = write.csv # Which R function we are using to save
-              , object = 'amb_RT_clean.csv' # Name of the file to save to (include file type)
+              , object = 'amb_RT_region.csv' # Name of the file to save to (include file type)
               , bucket = buck) # Bucket name defined above
 
+
+#Saving by trust 
+amb_dta_trusts<-amb_dta_clean %>% 
+  filter(org_code %in% c(list_org_codes_trust, "Eng")) %>% 
+  mutate(date=as.Date(paste0(year,"/",ifelse (month<10, paste0(0,month),month),"/",01))) 
+
+
+
+s3write_using(amb_dta_regions # What R object we are saving
+              , FUN = write.csv # Which R function we are using to save
+              , object = 'amb_RT_trusts.csv' # Name of the file to save to (include file type)
+              , bucket = buck) # Bucket name defined above
