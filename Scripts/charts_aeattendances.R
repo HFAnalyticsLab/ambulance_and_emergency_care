9a57c1fa782d485e1af4d7b9cc1e555ba9b4d59c
Mai Stafford (tier0) <tier0\mai.stafford@ec2amaz-yzaz13.tier0.prd.thf.aws>
2022-06-09 09:15:28 +0000

    Correct dates

--- Scripts/charts_aeattendances.R
+++ Scripts/charts_aeattendances.R
@@ -5,7 +5,8 @@ library(ggplot2)
 aeattend <- readRDS("aeattend.rds")
 
 aeattend <- aeattend %>%
-  mutate(time=paste0(year, month))
+  mutate(time=paste0(year, formatC(month, width=2, flag="0")))
+
 
 displaysummattend <- aeattend %>%
   group_by(`NHS England Region`, year, month) %>%
@@ -24,24 +25,29 @@ summattend <- aeattend %>%
 ggplot(data=summattend, aes(x=time, y=meanattend, group=`NHS England Region`)) +
   geom_line(aes(linetype=`NHS England Region`, colour=`NHS England Region`)) +
   ylab("Number of A&E attendances") +
-  ggtitle("Apr2018-Mar21")
+  ggtitle("Apr2018-Mar21") + 
+  theme(axis.text.x=element_text(angle=90, hjust=1))
 ggplot(data=summattend, aes(x=time, y=meantype1, group=`NHS England Region`)) +
   geom_line(aes(linetype=`NHS England Region`, colour=`NHS England Region`)) +
   ylab("Number of type 1 attendances") +
-  ggtitle("Apr2018-Mar21")
+  ggtitle("Apr2018-Mar21") + 
+  theme(axis.text.x=element_text(angle=90, hjust=1))
 ggplot(data=summattend, aes(x=time, y=meantype2, group=`NHS England Region`)) +
   geom_line(aes(linetype=`NHS England Region`, colour=`NHS England Region`)) +
   ylab("Number of type 2 attendances") +
-  ggtitle("Apr2018-Mar21")
+  ggtitle("Apr2018-Mar21") + 
+  theme(axis.text.x=element_text(angle=90, hjust=1))
 ggplot(data=summattend, aes(x=time, y=meantypeoth, group=`NHS England Region`)) +
   geom_line(aes(linetype=`NHS England Region`, colour=`NHS England Region`)) +
   ylab("Number of other attendances") +
-  ggtitle("Apr2018-Mar21")
+  ggtitle("Apr2018-Mar21") + 
+  theme(axis.text.x=element_text(angle=90, hjust=1))
 
 ggplot(data=summattend, aes(x=time, y=meanadmit, group=`NHS England Region`)) +
   geom_line(aes(linetype=`NHS England Region`, colour=`NHS England Region`)) +
   ylab("Number admitted from A&E") +
-  ggtitle("Apr2018-Mar21")
+  ggtitle("Apr2018-Mar21") + 
+  theme(axis.text.x=element_text(angle=90, hjust=1))
 
 
 summattend <- aeattend %>%
@@ -61,17 +67,20 @@ ggplot(data=summattend, aes(x=time, y=mean4pluswaittype1, group=`NHS England Reg
 ggplot(data=summattend, aes(x=time, y=mean4pluswaittype2, group=`NHS England Region`)) +
   geom_line(aes(linetype=`NHS England Region`, colour=`NHS England Region`)) +
   ylab("% waiting 4+ hrs in A&E type 2") +
-  ggtitle("Apr2018-Mar21")
+  ggtitle("Apr2018-Mar21") + 
+  theme(axis.text.x=element_text(angle=90, hjust=1))
 
 ggplot(data=summattend, aes(x=time, y=mean412admit, group=`NHS England Region`)) +
   geom_line(aes(linetype=`NHS England Region`, colour=`NHS England Region`)) +
   ylab("% waiting 4-12 hrs to be admitted from A&E") +
-  ggtitle("Apr2018-Mar21")
+  ggtitle("Apr2018-Mar21") + 
+  theme(axis.text.x=element_text(angle=90, hjust=1))
 
 ggplot(data=summattend, aes(x=time, y=mean12plusadmit, group=`NHS England Region`)) +
   geom_line(aes(linetype=`NHS England Region`, colour=`NHS England Region`)) +
   ylab("% waiting 12+ hrs to be admitted from A&E") +
-  ggtitle("Apr2018-Mar21")
+  ggtitle("Apr2018-Mar21") + 
+  theme(axis.text.x=element_text(angle=90, hjust=1))
 
 
 
@@ -80,45 +89,4 @@ ggplot(data=summattend, aes(x=time, group=`NHS England Region`)) +
   geom_line(aes(y=meanattend, colour=`NHS England Region`)) +
   geom_line(aes(y=meanadmit, colour=`NHS England Region`)) 
 
-
-ggplot(data=summattend, aes(x=time, y=meanattend, group=`NHS England Region`)) +
-  geom_line(aes(linetype=`NHS England Region`, colour=`NHS England Region`)) +
-  ylab("Number of A&E attendances") +
-  ggtitle("Apr2018-Mar21")
-
-
-
-
-
-
-summattend <- aeattend %>%
-  drop_na(totalattend) %>%
-  group_by(`NHS England Region`, time) %>%
-  summarise(meanattend=mean(totalattend), n=n()) 
-summattendt1 <- aeattend %>%
-  drop_na(`Number of A&E attendances Type 1`) %>%
-  group_by(`NHS England Region`, time) %>%
-  summarise(meantype1=mean(`Number of A&E attendances Type 1`), n=n())
-
-summadmit <- aeattend %>%
-  drop_na(totaladmit) %>%
-  group_by(`NHS England Region`, time) %>%
-  summarise(meanadmit=mean(totaladmit), n=n()) 
-summattend <- aeattend %>%
-  drop_na(totalattend) %>%
-  group_by(`NHS England Region`, time) %>%
-  summarise(meanattend=mean(totalattend), meanadmit=mean(totaladmit), meantype1=mean(`Number of A&E attendances Type 1`),
-            meantype2=mean(`Number of A&E attendances Type 2`), meantypeoth=mean(`Number of A&E attendances Other A&E Department`), n=n()) ## why is March 2019 data missing?
-
-
-
-ggplot(data=summocc, aes(x=time, y=meanoccup, group=`Region Code`)) +
-  geom_line(aes(linetype=`Region Code`, colour=`Region Code`)) +
-  ylim(0.5,1.0)
-
-ggplot(data=summamb, aes(x=month, y=mean3060)) +
-  geom_line(aes(linetype=`NHS England Region`, colour=`NHS England Region`)) +
-  ylim(0, 20) +
-  ylab("% handover 30-60mins") +
-  ggtitle("Dec2020-March2021")
 
