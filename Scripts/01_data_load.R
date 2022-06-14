28d67fd9ab098201ac8c50629a6af9799b6129eb
Anne Alarilla (tier0) <tier0\anne.alarilla@ec2amaz-yzaz13.tier0.prd.thf.aws>
2022-06-14 09:38:33 +0000

    add workforce data

--- Scripts/01_data_load.R
+++ Scripts/01_data_load.R
@@ -10,6 +10,9 @@ library(janitor)
 
 #data download
 
+
+#Ambulance Quality Indicators 
+
 link <- 'https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/05/AmbSYS-for-2022-Apr.csv'
 
 destfile <- here::here('data', "ambsys.csv")
@@ -17,7 +20,12 @@ curl_download(link, destfile = destfile)
 
 
 
+#NHS workforce statistics 
 
+link<-'https://files.digital.nhs.uk/95/096385/NHS%20Workforce%20Statistics%2C%20February%202022%20England%20and%20Organisation.xlsx'
+
+destfile <- here::here('data', "eng_workforce.xlsx")
+curl_download(link, destfile = destfile)
 
 
 
