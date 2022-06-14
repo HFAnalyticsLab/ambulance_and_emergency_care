e22252281052753f5f99dc0720a159f38805f5ce
Anne Alarilla (tier0) <tier0\anne.alarilla@ec2amaz-yzaz13.tier0.prd.thf.aws>
2022-06-14 12:31:45 +0000

    add England data

--- Scripts/05_workforce_data_cleaning.R
+++ Scripts/05_workforce_data_cleaning.R
@@ -13,22 +13,26 @@ library(dplyr)
 #data load 
 workforce_eng<-read_excel(here::here('data', "eng_workforce.xlsx"), sheet=2, skip=6)
 
-workforce_eng<-workforce_eng %>% 
+workforce_eng_clean<-workforce_eng %>% 
   clean_names() %>% 
   fill(england, .direction='down') %>% 
   mutate(x2=ifelse(is.na(england), "date", x2)) %>% 
-  filter(x2 %in% c("Ambulance staff", "date")) %>% 
+  filter(x2 %in% c("Ambulance staff", "date", "Total")) %>% 
   mutate(england=ifelse(is.na(england),"date",england)) %>% 
-  select(-x2) %>% 
-  t() %>% 
+  unite(met,england:x2, remove=FALSE) %>%
+  t() %>%
   row_to_names(row_number=1) %>% 
-  as.data.frame() 
-
-workforce_eng_clean<-workforce_eng %>% 
-  mutate(date=as.Date(as.numeric(date), origin = "1899-12-30")) %>% 
+  as.data.frame() %>% 
+  clean_names() %>% 
+  pivot_longer(c(headcount_total:full_time_equivalent_fte_ambulance_staff), names_to='metric', values_to='val') %>% 
+  mutate(group=ifelse(str_detect(metric, 'total'),"England", "Ambulance staff")) %>% 
+  mutate(met=ifelse(str_detect(metric,'headcount'),"headcount", "fte_count")) %>% 
+  mutate(date=as.Date(as.numeric(date_date), origin = "1899-12-30")) %>% 
   mutate(date=lubridate::date(date)) %>% 
+  mutate(val=as.numeric(val)) %>% 
   filter(!is.na(date)) %>% 
-  clean_names() 
+  clean_names() %>% 
+  select(c(date,group,met,val))
 
 #save workforce data 
 #### save R objects from the environment directly to your s3 bucket
