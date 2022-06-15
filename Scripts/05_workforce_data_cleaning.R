72fbd66e3d59a236c8bb00226112ffa03dcd37d3
Anne Alarilla (tier0) <tier0\anne.alarilla@ec2amaz-yzaz13.tier0.prd.thf.aws>
2022-06-15 12:11:05 +0000

    add turnover data

--- Scripts/05_workforce_data_cleaning.R
+++ Scripts/05_workforce_data_cleaning.R
@@ -45,4 +45,34 @@ s3write_using(workforce_eng_clean # What R object we are saving
 
 
 
+#Turnover data
+turnover<-read_excel(here::here('data', "eng_turnover.xlsx"), sheet=10)
+
+turnover<-turnover %>% 
+  clean_names() %>% 
+  select(choose_period:choose_region,staff_group:stability_index)
+  
+turnover_clean<-turnover %>% 
+  filter(choose_region=="England" & staff_group %in% 
+           c("All staff groups", "Ambulance staff","Support to ambulance staff")) %>% 
+  mutate(period=str_sub(turnover_clean$choose_period,5,6)) %>% 
+  mutate(year_start=ifelse(period %in% c(09,06),paste0(30,"-",period,"-",str_sub(turnover_clean$choose_period,3,4)),
+                           paste0(31,"-",period,"-",str_sub(turnover_clean$choose_period,3,4)))) %>%
+  mutate(year_end=ifelse(period %in% c(09,06),paste0(30,"-",period,"-",str_sub(turnover_clean$choose_period,13,14)),
+                                    paste0(31,"-",period,"-",str_sub(turnover_clean$choose_period,13,14)))) %>% 
+  mutate(year_start=as.Date(year_start, origin = "1899-12-30")) %>% 
+  mutate(year_start=lubridate::date(year_start)) %>% 
+  mutate(year_end=as.Date(year_end, origin = "1899-12-30")) %>% 
+  mutate(year_end=lubridate::date(year_end)) 
+
+buck <- 'thf-dap-tier0-projects-iht-067208b7-projectbucket-1mrmynh0q7ljp/ambulance/clean' ## my bucket name
+
+s3write_using(turnover_clean # What R object we are saving
+              , FUN = write.csv # Which R function we are using to save
+              , object = 'turnover_clean.csv' # Name of the file to save to (include file type)
+              , bucket = buck) # Bucket name defined above
+
+
+
+
 
