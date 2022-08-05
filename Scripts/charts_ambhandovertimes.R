6a8550384b50240a968797e5ad4dade77124144d
Anne Alarilla (tier0) <tier0\anne.alarilla@ec2amaz-yzaz13.tier0.prd.thf.aws>
2022-08-05 12:16:34 +0000

    add week start and year for the charts

--- Scripts/charts_ambhandovertimes.R
+++ Scripts/charts_ambhandovertimes.R
@@ -5,28 +5,38 @@ library(ggplot2)
 
 amball <- readRDS("amball201722.rds")
 
-amball <- amball %>%
-  mutate(monthyear=format(as.Date(date), "%Y-%m"))
- 
+
+date_1<-format(as.Date(seq(ymd('2017-11-20'),ymd('2018-03-04'),by='1 day')),"%Y-%m-%d")
+date_2<-format(as.Date(seq(ymd('2018-12-03'),ymd('2019-03-03'), by='1 day')),"%Y-%m-%d")
+date_3<-format(as.Date(seq(ymd('2019-12-02'),ymd('2020-03-01'), by='1 day')),"%Y-%m-%d")
+date_4<-format(as.Date(seq(ymd('2020-11-30'),ymd('2021-04-04'), by='1 day')),"%Y-%m-%d")
+date_5<-format(as.Date(seq(ymd('2021-11-29'),ymd('2022-04-03'), by='1 day')),"%Y-%m-%d")
   
+amball <- amball %>%
+  mutate(monthyear=format(as.Date(date), "%Y-%m")) %>% 
+  mutate(start_week=ISOweek2date(str_c(date2,"-1"))) %>% 
+  mutate(year=case_when(date %in% date_1~ "2017/18",
+                        date %in% date_2~ "2018/19",
+                        date %in% date_3~ "2019/20",
+                        date %in% date_4~ "2020/21",
+                        date %in% date_5~ "2021/22"))
+
 summamb <- amball %>%
   drop_na(pctdelay60plus) %>%
   drop_na(pctdelay3060) %>%
   group_by(date2) %>%
-  summarise(mean60plus=mean(pctdelay60plus), mean3060=mean(pctdelay3060), n=n())
+  mutate(mean60plus=mean(pctdelay60plus), mean3060=mean(pctdelay3060), n=n()) %>% 
+  select(c(start_week,date2, year,mean60plus,mean3060,n)) %>% 
+  distinct()
 
-summamb<-summamb %>% 
-  mutate(week_number=str_sub(date2,7,8)) %>% 
-  mutate(week_start = if_else(week_number >= 27,
-                              ISOweek2date(str_c("2020-W", str_pad(week_number, 2, side = "left", pad = "0"), "-1")),
-                              ISOweek2date(str_c("2021-W", str_pad(week_number, 2, side = "left", pad = "0"), "-1"))))
+
 # 
 # # drop months with fewer than 10 observations
 # summamb <- filter(summamb, n>9)
 
 p = ggplot(summamb) + 
-  geom_line(aes(week_start, mean60plus, group=1), color="red", lwd=2) +
-  geom_line(aes(week_start, mean3060, group=1), color="blue", lwd=2 ) +
+  geom_line(aes(start_week, mean60plus, group=1), color="red", lwd=2) +
+  geom_line(aes(start_week, mean3060, group=1), color="blue", lwd=2 ) +
   xlab("Month and year") +
   theme(axis.text.x=element_text(angle=90, hjust=1)) +
   ylab("% delayed")
