7cf9d854577638945156b50955ee3ea3167ed4df
Anne Alarilla (tier0) <tier0\anne.alarilla@ec2amaz-yzaz13.tier0.prd.thf.aws>
2022-08-05 12:42:29 +0000

    add ambulance handover data

--- Scripts/01_data_load.R
+++ Scripts/01_data_load.R
@@ -43,3 +43,40 @@ destfile <- here::here('data', "eng_sickness.csv")
 curl_download(link, destfile = destfile)
 
 
+#Ambulance handover delays
+
+#2017
+link<-'https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2018/03/Winter-data-Timeseries-20180304.xlsx'
+
+destfile <- here::here('data', "raw2017handovers.xlsx")
+curl_download(link, destfile = destfile)
+
+#2018
+link<-'https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2019/03/Winter-data-timeseries-20190307.xlsx'
+
+destfile <- here::here('data', "raw2018handovers.xlsx")
+curl_download(link, destfile = destfile)
+
+#2019
+link<-'https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2020/03/Winter-SitRep-Acute-Time-series-2-December-2019-1-March-2020.xlsx'
+
+destfile <- here::here('data', "raw2019handovers.xlsx")
+curl_download(link, destfile = destfile)
+
+#2020
+link<-'https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2021/04/UEC-Daily-SitRep-Acute-Web-File-Timeseries-1.xlsx'
+
+destfile <- here::here('data', "raw2020handovers.xlsx")
+curl_download(link, destfile = destfile)
+
+#2021
+link<-'https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/04/UEC-Daily-SitRep-Web-File-Timeseries.xlsx'
+
+destfile <- here::here('data', "raw2021handovers.xlsx")
+curl_download(link, destfile = destfile)
+
+
+
+
+
+
