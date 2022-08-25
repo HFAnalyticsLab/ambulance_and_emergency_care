6220b07a0745ebcd6c52f9a54890198f36aa68d3
Anne Alarilla (tier0) <tier0\anne.alarilla@ec2amaz-yzaz13.tier0.prd.thf.aws>
2022-08-25 10:14:26 +0000

    keep other org types

--- Scripts/05_workforce_data_cleaning.R
+++ Scripts/05_workforce_data_cleaning.R
@@ -74,15 +74,14 @@ s3write_using(turnover_clean # What R object we are saving
 #Staff sickness and absence rate 
 sick_ab<-read_csv(here::here('data', "eng_sickness.csv"))
 
-amb_sick_ab<-sick_ab %>% 
+sick_ab_all<-sick_ab %>% 
   clean_names() %>% 
-  filter(org_type=="Ambulance") %>% 
-  group_by(date) %>% 
+  group_by(date, org_type) %>% 
   summarise(across(where(is.numeric), sum)) %>% 
   select(-c(sort_date, sa_rate_percent)) %>% 
   mutate(sa_rate=round((fte_days_sick/fte_days_available)*100,2)) %>% 
-  mutate(date2=as.Date(paste0(date,"-01"), format="%Y-%b-%d")) %>% 
-  mutate(org_type="Ambulance")
+  mutate(date2=as.Date(paste0(date,"-01"), format="%Y-%b-%d"))
+
 
 
 eng_sick_ab<-sick_ab %>%
@@ -96,8 +95,10 @@ eng_sick_ab<-sick_ab %>%
   arrange(date2) %>% 
   mutate(org_type= "England")
 
-sick_ab_clean<-rbind(eng_sick_ab, amb_sick_ab)
+sick_ab_clean<-rbind(eng_sick_ab, sick_ab_all)
 
+sick_ab_clean<-sick_ab_clean %>% 
+  arrange(date2)
 
 buck <- 'thf-dap-tier0-projects-iht-067208b7-projectbucket-1mrmynh0q7ljp/ambulance/clean' ## my bucket name
 
