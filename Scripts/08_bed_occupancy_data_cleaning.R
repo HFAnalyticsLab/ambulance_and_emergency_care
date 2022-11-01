0f9d928cbdb16fd2b6b668bf0f1545c93493f374
Anne Alarilla (tier0) <tier0\anne.alarilla@ec2amaz-gjzwjg.tier0.prd.thf.aws>
2022-11-01 11:06:16 +0000

    update data extraction

--- Scripts/08_bed_occupancy_data_cleaning.R
+++ Scripts/08_bed_occupancy_data_cleaning.R
@@ -12,40 +12,57 @@ library(janitor)
 
 # Data load ---------------------------------------------------------------
 #N.B. Need to update the range to include latest data 
-bedoccup<-readxl::read_excel("data/overnightbedoccup.xls", sheet='Open Overnight', range="B14:O63")
-bedoccup_monthly<-readxl::read_excel("data/bedoccup_montly.xlsx", sheet='Timeseries type 1 acute trusts', range="B13:D44")
+
+bedoccup<-readxl::read_excel("data/overnightbedoccup.xls", sheet='Open Overnight')
+bedoccup_monthly<-readxl::read_excel("data/bedoccup_montly.xlsx", sheet='Timeseries type 1 acute trusts')
 
 
 
 # Formatting --------------------------------------------------------------
 
+#Quarterly 
+bedoccup<-bedoccup %>% 
+  clean_names() %>% 
+  slice(which(title=="Year"):n()) %>% 
+  row_to_names(., 1) %>% 
+  clean_names()
+
 bedoccup_clean<-bedoccup %>% 
   clean_names() %>% 
-  select(c(year, period,avail=general_acute_5, occup=general_acute_11)) %>% 
-  mutate(date=paste0(year," ", period),
+  select(c(year, period,avail=general_acute, occup=general_acute_2)) %>% 
+  mutate(avail=as.numeric(avail), 
+         occup=as.numeric(occup),
+         date=paste0(year," ", period),
          unoccup=avail-occup, 
          metric="Quarterly") %>% 
-  filter(year %in% c("2017/18", "2018/19", "2019/20", "2020/21", "2021/22", "2022/23"))
+  filter(year %in% c("2017/18", "2018/19", "2019/20", "2020/21", "2021/22", "2022/23")) %>% 
+  na.omit()
   
 #write.csv(bedoccup_clean, 'bed_occup_quarters.csv')
 
+
+
+#Monthly 
+
+bedoccup_monthly<-bedoccup_monthly %>% 
+  clean_names() %>% 
+  slice(which(title=="Month"):n()) %>% 
+  row_to_names(., 1) %>% 
+  clean_names()
+
 bedoccup_monthly_clean<-bedoccup_monthly %>% 
   clean_names() %>% 
-  rename(avail=g_a_beds_available,occup=g_a_beds_occupied, date=month) %>% 
-  mutate(unoccup=avail-occup, 
+  select(avail=g_a_beds_available,occup=g_a_beds_occupied, date=month) %>% 
+  mutate(avail=as.numeric(avail), 
+         occup=as.numeric(occup),
+         unoccup=avail-occup, 
          metric="Monthly") %>% 
   mutate(date2=as.Date(paste0("01 ",date), format="%d %B %Y")) %>% 
-  mutate(monthyear=format(as.Date(date2), "%b %y")) 
+  mutate(monthyear=format(as.Date(date2), "%b %y")) %>% 
+  na.omit()
 
 #write.csv(bedoccup_monthly_clean, 'bedoccup_monthly.csv')
 
-
-#Combining monthly with quarterly data 
-
-bed_occupancy<-bedoccup_clean %>% 
-  full_join(bedoccup_monthly_clean)
-
-#write.csv(bed_occupancy, 'bed_occupancy_full.csv')
 
 # Save data ---------------------------------------------------------------
 #Only saving monthly data 
